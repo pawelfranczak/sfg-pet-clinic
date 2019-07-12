4b6adda71f4935e1e9d2872b7afa3c08b3153ced
Pawel Franczak <gzem89@gmail.com>
2019-07-12 14:21:55 +0200

    Refactor Services using CrudService interface

--- pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/services/PetService.java
@@ -2,14 +2,9 @@ package pl.pfranczak.sfgpetclinic.services;
 
 import java.util.Set;
 
+import pl.pfranczak.sfgpetclinic.model.Owner;
 import pl.pfranczak.sfgpetclinic.model.Pet;
 
-public interface PetService {
-
-	Pet findById(Long id);
-	
-	Pet save(Pet Pet);
-	
-	Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 	
 }
--- pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/services/VetService.java
@@ -1,15 +1,7 @@
 package pl.pfranczak.sfgpetclinic.services;
 
-import java.util.Set;
+import pl.pfranczak.sfgpetclinic.model.Owner;
 
-import pl.pfranczak.sfgpetclinic.model.Vet;
-
-public interface VetService {
-
-	Vet findById(Long id);
-	
-	Vet save(Vet Vet);
-	
-	Set<Vet> findAll();
+public interface VetService extends CrudService<Owner, Long> {
 	
 }
