1212d0e7353d623836cb40d8bf8c153a543a7ec0
Pawel Franczak <gzem89@gmail.com>
2019-07-12 14:37:45 +0200

    Add id field

--- pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Person.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package pl.pfranczak.sfgpetclinic.model;
 
 public class Person extends BaseEntity {
@@ -19,3 +20,35 @@ public class Person extends BaseEntity {
 	}
 
 }
+=======
+package pl.pfranczak.sfgpetclinic.model;
+
+public class Person {
+	
+	Long id;
+	String firstName;
+	String lastName;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
+>>>>>>> Add id field
