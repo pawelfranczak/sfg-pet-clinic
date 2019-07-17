a5240f2180aa1b36e3b3ff1d0ee7b77889bd5899
Pawel Franczak <gzem89@gmail.com>
2019-07-17 15:41:51 +0200

    fix request mapping for owners

--- pet-clinic-web/src/main/java/pl/pfranczak/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/pl/pfranczak/sfgpetclinic/controllers/OwnersController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class OwnersController {
 
-	@RequestMapping({"/", "/index", "/index.html"})
+	@RequestMapping({"", "/", "/index", "/index.html"})
 	public String listoOwners() {
 		return "owners/index";
 	}
