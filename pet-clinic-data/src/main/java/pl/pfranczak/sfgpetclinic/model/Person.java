84fa0a5336083107058e6bc812341a2b75786b02
Pawel Franczak <gzem89@gmail.com>
2019-07-17 15:31:31 +0200

    cleanup

--- pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Person.java
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-package pl.pfranczak.sfgpetclinic.model;
-
-public class Person extends BaseEntity {
-	
-	String firstName;
-	String lastName;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-}
-=======
 package pl.pfranczak.sfgpetclinic.model;
 
 public class Person {
@@ -51,4 +28,3 @@ public class Person {
 	}
 
 }
->>>>>>> Add id field
--- pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/pl/pfranczak/sfgpetclinic/model/Pet.java
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-package pl.pfranczak.sfgpetclinic.model;
-
-import java.time.LocalDate;
-
-public class Pet extends BaseEntity {
-
-	private PetType perType;
-	private Owner owner;
-	private LocalDate birthDate;
-	
-	public PetType getPerType() {
-		return perType;
-	}
-	public void setPerType(PetType perType) {
-		this.perType = perType;
-	}
-	public Owner getOwner() {
-		return owner;
-	}
-	public void setOwner(Owner owner) {
-		this.owner = owner;
-	}
-	public LocalDate getBirthDate() {
-		return birthDate;
-	}
-	public void setBirthDate(LocalDate birthDate) {
-		this.birthDate = birthDate;
-	}
-	
-}
-=======
 package pl.pfranczak.sfgpetclinic.model;
 
 import java.time.LocalDate;
@@ -68,4 +36,3 @@ public class Pet {
 	}
 	
 }
->>>>>>> Create map services
