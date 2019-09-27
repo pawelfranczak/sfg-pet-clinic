a958fc265a78357ad0c0d882af25330e663470a8
Pawel Franczak <gzem89@gmail.com>
2019-09-27 08:53:05 +0200

    Spring Guru Lesson 90

--- pet-clinic-web/src/main/java/pl/pfranczak/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/pl/pfranczak/sfgpetclinic/bootstrap/DataLoader.java
@@ -44,9 +44,9 @@ public class DataLoader implements CommandLineRunner{
 		vetService.save(vet1);
 		
 		Vet vet2 = new Vet();
-		vet1.setId(2L);
-		vet1.setFirstName("Pawel");
-		vet1.setLastName("Porter");
+		vet2.setId(2L);
+		vet2.setFirstName("Pawel");
+		vet2.setLastName("Porter");
 		vetService.save(vet2);
 		
 		System.out.println("Loaded Vets....");
